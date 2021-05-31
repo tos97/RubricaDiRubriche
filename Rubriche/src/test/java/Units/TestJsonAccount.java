82fc41b5f2d3df24949d2113846f6a0601839b54
tos97 <47986842+tos97@users.noreply.github.com>
2021-05-31 16:55:39 +0200

    boh

--- Rubriche/src/test/java/Units/TestJsonAccount.java
+++ Rubriche/src/test/java/Units/TestJsonAccount.java
@@ -8,13 +8,14 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class TestJsonAccount {
     MenuRubrica rub = new MenuRubrica();
-    ArrayList<Account> rubrica;
+    ArrayList<Models.Account> rubrica;
     private static Account account_1;
     private static Account account_2;
     private static final String json1 = "[{\"user\":{\"nome\":\"francesco\",\"surname\":\"fusco\",\"age\":\"21\",\"uid\":\"7b7949f2-e97f-4ce1-b268-966ebb108ae4\"},\"numero\":\"1234567890\",\"email\":\"fusco.com\"}]";
@@ -23,13 +24,13 @@ public class TestJsonAccount {
     //"[{\"user\":{\"nome\":\"michele\",\"surname\":\"rubino\",\"age\":\"\",\"uid\":\"30a32ef7-c5e2-44b5-a403-51385b5e8928\"},\"numero\":\"0987654321\",\"email\":\"\"}]\""
 
     @BeforeAll
-    static void beforeAll(){
-        account_1 = new Account(new User("francesco","fusco","21","7b7949f2-e97f-4ce1-b268-966ebb108ae4"),"1234567890","fusco.com");
-        account_2 = new Account(new User("michele","rubino","","30a32ef7-c5e2-44b5-a403-51385b5e8928"),"0987654321","");
+    static void beforeAll() {
+        account_1 = new Account(new User("francesco", "fusco", "21", "7b7949f2-e97f-4ce1-b268-966ebb108ae4"), "1234567890", "fusco.com");
+        account_2 = new Account(new User("michele", "rubino", "", "30a32ef7-c5e2-44b5-a403-51385b5e8928"), "0987654321", "");
     }
 
     @BeforeEach
-    void beforeEach(){
+    void beforeEach() {
         rubrica = new ArrayList<>();
     }
 
@@ -37,16 +38,16 @@ public class TestJsonAccount {
     //@ValueSource (strings = {json1, json2})
     @Test
     @DisplayName("Salva Json")
-    void Test_001_Salva(){
+    void Test_001_Salva() {
         rubrica.addAll(Arrays.asList(rub.importJ(json1)));
-        ArrayList<Account> temp = new ArrayList<>();
+        ArrayList<Models.Account> temp = new ArrayList<>();
         temp.add(account_1);
-        temp.add(account_2);
+        //temp.add(account_2);
+        boolean status = false;
         int c = 0;
-        for (Account contenuto1: rubrica){
-            int i = 0;
-            for (Account contenuto2: temp){
-                if(contenuto1.getUser().getNome().equals(contenuto2.getUser().getNome()))
+        for (int i = 0; i < temp.size(); i++) {
+        assertThat(temp.get(i)).isEqualToComparingFieldByField(rubrica.get(i));
+                /*if(contenuto1.getUser().getNome().equals(contenuto2.getUser().getNome()))
                     i++;
                 if(contenuto1.getUser().getSurname().equals(contenuto2.getUser().getSurname()))
                     i++;
@@ -57,13 +58,14 @@ public class TestJsonAccount {
                 if(contenuto1.getEmail().equals(contenuto2.getEmail()))
                     i++;
                 if(contenuto1.getNumero().equals(contenuto2.getNumero()))
-                    i++;
-            }
-            if (i == 6)
-                c++;
-        }
-        assertEquals(c, rubrica.size());
+                    i++;*/
+
+        //if (i == 6)
+        //   c++;
     }
+    //assertTrue(status);
+    //assertEquals(c, rubrica.size());
+}
 
     @Test
     @DisplayName("Export Json")
